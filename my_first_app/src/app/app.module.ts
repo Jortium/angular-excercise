e814bdd465c8b6ddb692967c60c545e4e49d1b95
Josh Ortiz <joshua_w_ortiz@keybank.com>
2021-08-30 14:09:15 -0600

    Lesson 27 progress

--- my_first_app/src/app/app.module.ts
+++ my_first_app/src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { AlertComponent } from './alert/alert.component';
 
 
 @NgModule({
@@ -14,9 +11,6 @@ import { AlertComponent } from './alert/alert.component';
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    AlertComponent
   ],
   imports: [
     BrowserModule,
--- my_first_app/src/app/servers/servers.component.ts
+++ my_first_app/src/app/servers/servers.component.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  constructor(){
+    setTimeout(()=> { this.allowNewServer = true})
+   }
 
   ngOnInit(): void {
   }
