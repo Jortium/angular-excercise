2f6e59cfa010c74ebc0b28a420b1f47a14c0cb37
Josh Ortiz <joshua_w_ortiz@keybank.com>
2021-08-30 11:14:40 -0600

    completed 1st excercise

--- my_first_app/src/app/servers/servers.component.ts
+++ my_first_app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+  // Selector works similarly as CSS we can also use [app-server] to make it select attribute or for class we can use .
+  // In the HTML portion to select these an example may be <div app-servers></div> using [] as the selector
+  selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
